0d3b2b723dfa7f1387afacf87986678e9a2cd09c
xmartina <martinsprince126@gmail.com>
2023-10-12 16:00:49 +0100

    fixed

--- include/smtp.php
+++ include/smtp.php
@@ -10,18 +10,18 @@ class message{
         $mail = new PHPMailer();
         //SMTP Settings
         $mail->isSMTP();
-        $mail->Host = "mail.westmonument.online"; // Change Email Host
+        $mail->Host = "mail.azurecreditunion.com"; // Change Email Host
         $mail->SMTPAuth = true;
-        $mail->Username = "info@westmonument.online"; // Change Email Address
-        $mail->Password = '@SECwestmonument1'; // Change Email Password
+        $mail->Username = "info@azurecreditunion.com"; // Change Email Address
+        $mail->Password = '@SECazurecreditunion1'; // Change Email Password
         $mail->Port = 587; //587
         $mail->SMTPSecure = "ssl"; //tls
 
         //Email Settings
         $mail->isHTML(true);
-        $mail->setFrom('info@westmonument.online','West Monument Bank'); // Change
+        $mail->setFrom('contact@azurecreditunion.com','Azure Credit Union'); // Change
         $mail->addAddress($email);
-        $mail->AddReplyTo("info@westmonument.online", "West Monument Bank"); // Change
+        $mail->AddReplyTo("contact@azurecreditunion.com", "Azure Credit Union"); // Change
         $mail->Subject = $subject;
         $mail->MsgHTML($message);
         $mail->Send();
